05647a34a83d6af22612d71f58ce8d1b0e4efb41
Manish8951 <manish.xworkz@gmail.com>
2023-01-05 23:40:42 +0530

    tocompare method using in collection

--- collection2/src/com/source/runner/WeaponRunner.java
+++ collection2/src/com/source/runner/WeaponRunner.java
@@ -17,7 +17,7 @@ public class WeaponRunner {
 		
 		WeaponDTO dto=new  WeaponDTO("A1-47","Avinash","Metal",170000,Type.AK47);
 		WeaponDTO dto1=new  WeaponDTO("Tanker","longer world","alluminum",1000,Type.M416);
-		WeaponDTO dto2=new  WeaponDTO("Rocket Launcer","Wrold war","Platinum",176000,Type.MA47);
+		WeaponDTO dto2=new  WeaponDTO("Rocket Launcher","Wrold war","Platinum",176000,Type.MA47);
 		WeaponDTO dto3=new  WeaponDTO("F16-weapon","lord of rings","Gold",350000,Type.AK47);
 		WeaponDTO dto4=new  WeaponDTO("Panzer","Netflix","Tagad",10,Type.M416);
 		
@@ -47,21 +47,13 @@ public class WeaponRunner {
 	    System.out.println(System.lineSeparator());
 	    dtos.stream().sorted((a1,a2)->Double.compare(a2.getPrice(),a1.getPrice())).forEach(e->System.out.println(e.getPrice()));
 	   
+	
+		System.out.println(System.lineSeparator());
+		dtos.stream().sorted((a1,a2)->(a1.getName()+a1.getMadeOn()).compareTo(a2.getName()+a2.getMadeOn())).forEach(e->System.out.println(e.getName()+"--"+e.getMadeOn()
+		));
 		
-	    System.out.println(System.lineSeparator());
-		dtos.stream().sorted((a1,a2)->a1.getName().compareTo(a2.getName())).forEach(e->System.out.println(e.getName()));
-		dtos.stream().sorted((a1,a2)->a1.getMadeOn().compareTo(a2.getMadeOn())).forEach(e->System.out.println(e.getMadeOn()));
-		
-		
-	    System.out.println(System.lineSeparator());
-		dtos.stream().sorted((a1,a2)->a2.getType().compareTo(a1.getType())).forEach(e->System.out.println(e.getType()));
-		dtos.stream().sorted((a1,a2)->a2.getMadeBy().compareTo(a2.getMadeBy())).forEach(e->System.out.println(e.getMadeBy()));
-			
-		
-		
-		
-		
-		
+		System.out.println(System.lineSeparator());
+		dtos.stream().sorted((a1,a2)->(a2.getType()+a2.getMadeBy()+a2.getName()).compareTo(a1.getType()+a1.getMadeBy()+a1.getName())).forEach(e->System.out.println(e.getType()+"--"+e.getMadeBy()+"--"+e.getName()));
 		
 		
 		
